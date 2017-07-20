8eca5088354afded3be8ad3d371a90df14e28fc2
yotam shalev <yotamsha@gmail.com>
2017-07-20 15:32:51 +0300

    Added a basic Sails REST server with a single Drawing model.

---
+++ server/api/controllers/DrawingController.js
@@ -0,0 +1,11 @@
+/**
+ * DrawingController
+ *
+ * @description :: Server-side logic for managing drawings
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
