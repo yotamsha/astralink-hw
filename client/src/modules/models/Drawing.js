f6cc882a4b21e58d7434881077bd0e704f0b264f
yotam shalev <yotamsha@gmail.com>
2017-07-22 23:48:21 +0300

    Another refactor for base API url

--- client/src/modules/models/Drawing.js
+++ client/src/modules/models/Drawing.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 import Consts from '../services/Consts';
 
 const MODEL_PATH = 'drawings';
-
+const BASE_PATH = Consts.API_PREFIX + MODEL_PATH
 class Drawing {
 
   static getAll(){
-    return axios.get(Consts.API_PREFIX + MODEL_PATH)
+    return axios.get(BASE_PATH)
       .then(res => {
         return res.data;
       });
   }
 
   static getById(id){
-    return axios.get(BASE_SERVER_URL + MODEL_PATH + '/' + id)
+    return axios.get(BASE_PATH + '/' + id)
       .then(res => {
         return res.data;
       });
@@ -22,12 +22,12 @@ class Drawing {
 
   static save(data){
     if (data.id){ // ID exists,
-      return axios.put(BASE_SERVER_URL + MODEL_PATH + '/' + data.id, data)
+      return axios.put(BASE_PATH + '/' + data.id, data)
         .then(res => {
           return res.data;
         });
     } else { // no ID, create a new object.
-      return axios.post(BASE_SERVER_URL + MODEL_PATH, data)
+      return axios.post(BASE_PATH, data)
         .then(res => {
           return res.data;
         });
